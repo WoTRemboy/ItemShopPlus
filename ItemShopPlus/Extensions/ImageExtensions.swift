4ad6e5f02fcb81baa1ff9d09b880e9390ee3fe33
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2023-12-16 14:40:30 +0300

    main buttons images changed

--- ItemShopPlus/Extensions/ImageExtensions.swift
+++ ItemShopPlus/Extensions/ImageExtensions.swift
@@ -13,13 +13,30 @@ extension UIImage {
     }
     
     enum MainButtons {
-        static let shop = UIImage(named: "Shop")
-        static let news = UIImage(named: "News")
-        static let tournaments = UIImage(named: "Tournaments")
-        static let quests = UIImage(named: "Quests")
+        static let shop = createImage(name: "basket")
+        static let news = createImage(name: "newspaper")
+        static let tournaments = createImage(name: "medal")
+        static let quests = createImage(name: "list.bullet.clipboard")
+        static let crew = createImage(name: "waveform")
+        static let map = createImage(name: "map")
+        static let vehicles = createImage(name: "car")
+        static let augments = createImage(name: "square.3.layers.3d.bottom.filled")
+        
+//        static let shop = UIImage(named: "Shop")
+//        static let news = UIImage(named: "News")
+//        static let tournaments = UIImage(named: "Tournaments")
+//        static let quests = UIImage(named: "Quests")
     }
     
     enum Quests {
         static let experience = UIImage(named: "QuestXP")
     }
+}
+
+private func createImage(name: String) -> UIImage? {
+    let image = UIImage(
+        systemName: name,
+        withConfiguration: UIImage.SymbolConfiguration(
+            paletteColors: [.labelTertiary, .labelTertiary]))
+    return image
 }

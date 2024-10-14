25f806072ee34d0d02e77bfc117a81e94194aef6
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2024-10-14 14:44:06 +0300

    favourites module documentation & comments added

--- ItemShopPlus/FavouritesModule/View/FavouritesFooterReusableView.swift
+++ ItemShopPlus/FavouritesModule/View/FavouritesFooterReusableView.swift
@@ -7,18 +7,24 @@
 
 import UIKit
 
+/// A reusable view that displays the total price in the Favourites section
 final class FavouritesFooterReusableView: UICollectionReusableView {
     
     // MARK: - Properties
     
+    /// The identifier for this reusable view
     static let identifier = Texts.FavouritesPage.footerIdentifier
+    /// The position of the currency symbol (e.g., left or right)
     private var symbolPosition: CurrencySymbolPosition = .left
+    /// A constraint used to dynamically adjust the width of the price view
     private var priceWidthAnchor: NSLayoutConstraint = .init()
     
     // MARK: - UI Elements and Views
     
+    /// A custom view that shows the total price
     private let priceView = CollectionTotalPriceView(frame: .null, price: Texts.ShopGrantedCell.price)
     
+    /// A label that displays the "Total" text above the price
     private let totalPriceLabel: UILabel = {
         let label = UILabel()
         label.text = Texts.ShopGrantedCell.total
@@ -30,11 +36,15 @@ final class FavouritesFooterReusableView: UICollectionReusableView {
     
     // MARK: - Public Configure Methods
     
+    /// Configures the footer view with the initial price
+    /// - Parameter price: The initial price to display
     public func configurate(price: Int) {
         priceView.changePrice(price: price, animated: false)
         totalPriceSetup(price: price)
     }
     
+    /// Updates the price displayed in the footer view
+    /// - Parameter price: The new price to update
     public func changePrice(to price: Int) {
         priceWidthAnchor = priceView.widthAnchor.constraint(equalToConstant: CGFloat(25 + 10 + (18 * String(price).count)))
         priceWidthAnchor.isActive = true
@@ -47,6 +57,8 @@ final class FavouritesFooterReusableView: UICollectionReusableView {
     
     // MARK: - UI Setups
     
+    /// Sets up the layout and constraints for the total price label and the price view
+    /// - Parameter price: The initial price to be displayed
     private func totalPriceSetup(price: Int) {
         addSubview(totalPriceLabel)
         addSubview(priceView)

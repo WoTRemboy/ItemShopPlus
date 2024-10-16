a40972e7e998c6a414d9bb6d90977cf503e5b8cb
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2024-10-16 12:16:38 +0300

    data base module documentation added

--- ItemShopPlus/DataBase/Manager/CoreDataManager.swift
+++ ItemShopPlus/DataBase/Manager/CoreDataManager.swift
@@ -8,23 +8,34 @@
 import Foundation
 import CoreData
 
+/// A class responsible for managing the Core Data stack, specifically for saving and retrieving data related to `FavouriteShopItem`
 final class CoreDataManager {
+    /// Shared instance for accessing the `CoreDataManager`
     static let shared = CoreDataManager()
+    /// Private initializer to ensure the singleton pattern
     private init() {}
     
-    lazy var persistentContainer: NSPersistentContainer = {
+    // MARK: - Persistent Container
+    
+    /// The persistent container that manages the Core Data stack
+    internal lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "FavouriteShopItem")
+        // Load the persistent store and handle errors if any
         container.loadPersistentStores {_, error in
             _ = error.map { fatalError("Persistent container (Favourites DB) error: \($0)") }
         }
         return container
     }()
     
-    var mainContext: NSManagedObjectContext {
+    // MARK: - Context Methods
+    
+    /// The main context for interacting with the Core Data store
+    internal var mainContext: NSManagedObjectContext {
         return persistentContainer.viewContext
     }
     
-    func saveContext() {
+    /// Saves any changes made in the main context to the persistent store
+    internal func saveContext() {
         let context = mainContext
         if context.hasChanges {
             do {
--- ItemShopPlus/DataBase/Manager/FavouritesDataBaseManager.swift
+++ ItemShopPlus/DataBase/Manager/FavouritesDataBaseManager.swift
@@ -8,15 +8,26 @@
 import Foundation
 import CoreData
 
+/// Manages the storage and retrieval of favorite shop items in the Core Data database
 final class FavouritesDataBaseManager {
+    /// Holds the array of shop items stored in the database
     private(set) var items = [ShopItem]()
+    /// Singleton instance for the manager
     static let shared = FavouritesDataBaseManager()
     
-    init() {
+    /// Initializes the manager and loads data from the database
+    internal init() {
         loadFromDataBase()
     }
     
-    func fetchItem(withID id: String, in context: NSManagedObjectContext) -> FavouriteShopItemEntity? {
+    // MARK: - Fetching Methods
+    
+    /// Fetches an item from the database based on its ID
+    /// - Parameters:
+    ///   - id: The ID of the shop item to be fetched
+    ///   - context: The `NSManagedObjectContext` in which the fetch request is performed
+    /// - Returns: A `FavouriteShopItemEntity` if found, otherwise `nil`
+    internal func fetchItem(withID id: String, in context: NSManagedObjectContext) -> FavouriteShopItemEntity? {
         let fetchRequest: NSFetchRequest<FavouriteShopItemEntity> = FavouriteShopItemEntity.fetchRequest()
         fetchRequest.predicate = NSPredicate(format: "id == %@", id)
         
@@ -28,7 +39,14 @@ final class FavouritesDataBaseManager {
             return nil
         }
     }
+    
+    // MARK: - Creation Methods
 
+    /// Creates a Core Data shop item entity from a `ShopItem` model
+    /// - Parameters:
+    ///   - item: The `ShopItem` model to be stored
+    ///   - context: The `NSManagedObjectContext` in which the entity will be created
+    /// - Returns: A configured `FavouriteShopItemEntity`
     private func createCoreDataShopItem(from item: ShopItem, in context: NSManagedObjectContext) -> FavouriteShopItemEntity {
         let coreDataItem = FavouriteShopItemEntity(context: context)
         coreDataItem.id = item.id
@@ -57,6 +75,12 @@ final class FavouritesDataBaseManager {
         return coreDataItem
     }
     
+    /// Creates a Core Data granted item entity from a `GrantedItem` model
+    /// - Parameters:
+    ///   - item: The `GrantedItem` model to be stored
+    ///   - parent: The parent `FavouriteShopItemEntity`
+    ///   - context: The `NSManagedObjectContext` in which the entity will be created
+    /// - Returns: A configured `ShopGrantedItemEntity`
     private func createCoreDataGrantedItem(from item: GrantedItem, parent: FavouriteShopItemEntity, in context: NSManagedObjectContext) -> ShopGrantedItemEntity {
         
         let coreDataItem = ShopGrantedItemEntity(context: context)
@@ -75,6 +99,13 @@ final class FavouritesDataBaseManager {
         return coreDataItem
     }
     
+    /// Creates a Core Data image entity from a `ShopItemImage` model
+    ///
+    /// - Parameters:
+    ///   - item: The `ShopItemImage` model to be stored
+    ///   - parent: The parent `FavouriteShopItemEntity`
+    ///   - context: The `NSManagedObjectContext` in which the entity will be created
+    /// - Returns: A configured `ShopItemImageEntity`
     private func createCoreDataShopImage(from item: ShopItemImage, parent: FavouriteShopItemEntity, in context: NSManagedObjectContext) -> ShopItemImageEntity {
         
         let coreDataItem = ShopItemImageEntity(context: context)
@@ -85,7 +116,11 @@ final class FavouritesDataBaseManager {
         return coreDataItem
     }
     
-    func insertToDataBase(item: ShopItem) {
+    // MARK: - Database Operations
+    
+    /// Inserts a new shop item into the Core Data database
+    /// - Parameter item: The `ShopItem` to be inserted into the database
+    internal func insertToDataBase(item: ShopItem) {
         CoreDataManager.shared.persistentContainer.performBackgroundTask { context in
             let _ = self.createCoreDataShopItem(from: item, in: context)
             
@@ -98,7 +133,9 @@ final class FavouritesDataBaseManager {
         }
     }
     
-    func removeFromDataBase(at itemID: String) {
+    /// Removes a shop item from the Core Data database based on its ID
+    /// - Parameter itemID: The ID of the shop item to be removed
+    internal func removeFromDataBase(at itemID: String) {
         CoreDataManager.shared.persistentContainer.performBackgroundTask { context in
             if let existingItem = self.fetchItem(withID: itemID, in: context) {
                 context.delete(existingItem)
@@ -113,7 +150,8 @@ final class FavouritesDataBaseManager {
         }
     }
     
-    func loadFromDataBase() {
+    /// Loads all shop items from the Core Data database into the `items` array
+    internal func loadFromDataBase() {
         let context = CoreDataManager.shared.mainContext
         let fetchRequest: NSFetchRequest<FavouriteShopItemEntity> = FavouriteShopItemEntity.fetchRequest()
         
